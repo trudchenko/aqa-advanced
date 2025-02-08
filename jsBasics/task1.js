ed63a40f691c5c0f6a32c19ea7d6d7eb57007a64
Tetiana Rudchenko <rudchenkosumy@gmail.com>
2025-02-08 17:13:11 +0200

    Lesson 4

---
+++ jsBasics/task1.js
@@ -0,0 +1,12 @@
+let stringVar = "Hello";
+let numVar = 5;
+let boolVar = true;
+let nullVar = null;
+let undefVar = undefined;
+
+console.log(stringVar);
+console.log(numVar);
+console.log(boolVar);
+console.log(nullVar);
+console.log(undefVar);
+
