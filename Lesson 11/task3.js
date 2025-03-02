97a248b29e106d8b5913399764da7a3812826bda
Tetiana Rudchenko <rudchenkosumy@gmail.com>
2025-03-02 23:46:22 +0200

    Lesson 11

---
+++ Lesson 11/task3.js
@@ -0,0 +1,26 @@
+// Виконайте завдання 2 але використовуючи конструкцію async/await для функції що виконує запити
+
+async function fetchUrl(url) {
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch (error) {
+        return { error: error.message };
+    }
+}
+
+function fetchToDo () {
+    return fetchUrl('https://jsonplaceholder.typicode.com/todos/1');
+}
+
+function fetchUser () {
+    return fetchUrl('https://jsonplaceholder.typicode.com/users/1');
+}
+
+const promiseArray = [fetchToDo(), fetchUser()];
+
+let promiseAll = await Promise.all(promiseArray);
+console.log(promiseAll);
+
+let promiseRace = await Promise.race(promiseArray);
+console.log(promiseRace);
